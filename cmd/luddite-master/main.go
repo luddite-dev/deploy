a441ac22c31a75c68f4c947b236e95867ad9ef2d
Antonio Cheong <acheong@duti.dev>
2026-06-19 20:43:44 +0800

    feat: add --help and flags to luddite-master

--- cmd/luddite-master/main.go
+++ cmd/luddite-master/main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
+	"github.com/luddite-dev/deploy/internal/envutil"
 	masterhttp "github.com/luddite-dev/deploy/internal/master/httpapi"
 	"github.com/luddite-dev/deploy/internal/master/state"
 	"github.com/luddite-dev/deploy/internal/sidecar/client"
 )
 
 func main() {
-	store, err := state.Open(os.Getenv("LUDDITE_MASTER_STATE"))
+	statePath := flag.String("state",
+		envutil.EnvOrDefault("LUDDITE_MASTER_STATE", "luddite-master.state.json"),
+		"path to the master state file (env LUDDITE_MASTER_STATE)")
+	sidecarAddr := flag.String("sidecar",
+		envutil.EnvOrDefault("LUDDITE_MASTER_SIDECAR", "127.0.0.1:7777"),
+		"address of the local iroh-bridge sidecar (env LUDDITE_MASTER_SIDECAR)")
+	flag.Parse()
+
+	store, err := state.Open(*statePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	sidecar := client.New(os.Getenv("LUDDITE_MASTER_SIDECAR"))
+	sidecar := client.New(*sidecarAddr)
 	masterEndpointAddr, err := sidecar.Identity(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("luddite-master: state=%s sidecar=%s http=:8080 master-addr=%s",
+		*statePath, *sidecarAddr, masterEndpointAddr)
 
 	go func() {
 		for {
